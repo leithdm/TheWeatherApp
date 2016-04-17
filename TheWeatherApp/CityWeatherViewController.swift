bc0db2227d9249e04f698a64ebe8332dc6ea9d25
leithdm@gmail.com <darrenm.leith@lethalapps.com>
2016-04-17 10:31:32 +0100

    feat: User can scroll through a weekly forecast

--- TheWeatherApp/CityWeatherViewController.swift
+++ TheWeatherApp/CityWeatherViewController.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class CityWeatherViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class CityWeatherViewController: UIViewController {
+	
+	//MARK: properties
 	
 	var city: City!
-//	var days = [Day]()
-
+	
+	//MARK:	outlets
+	
 	@IBOutlet weak var cityName: UILabel!
 	@IBOutlet weak var weatherDescription: UILabel!
 	@IBOutlet weak var mainTempValue: UILabel!
 	@IBOutlet weak var collectionView: UICollectionView!
+	@IBOutlet weak var tableView: UITableView!
 	
 	
-	// MARK: - Life Cycle
+	// MARK: life cycle methods
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -28,7 +32,7 @@ class CityWeatherViewController: UIViewController, UICollectionViewDataSource, U
 	override func viewDidLayoutSubviews() {
 		super.viewDidLayoutSubviews()
 		
-		// Lay out the collection view so that cells take up 1/3 of the width
+		// Lay out the collection view so that cells take up 1/4 of the width
 		let width = CGRectGetWidth(view.frame) / 4
 		let layout = collectionView!.collectionViewLayout as! UICollectionViewFlowLayout
 		layout.itemSize = CGSize(width: width, height: width) //want them to be square
@@ -38,8 +42,9 @@ class CityWeatherViewController: UIViewController, UICollectionViewDataSource, U
 		super.viewWillAppear(animated)
 		
 		cityName.text = city.name
-
-		let parameters = ["q" : city.name]
+		city.forecast.removeAll()
+		
+		let parameters = ["q": city.name]
 		
 		APIWeatherOnline.sharedInstance().taskForResource(parameters) { [unowned self] jsonResult, error in
 			
@@ -59,7 +64,7 @@ class CityWeatherViewController: UIViewController, UICollectionViewDataSource, U
 				return
 			}
 			
-			//weather for HUD
+			//weather for main display
 			for current in current_condition {
 				
 				//todays temperature in C
@@ -96,10 +101,10 @@ class CityWeatherViewController: UIViewController, UICollectionViewDataSource, U
 					return
 				}
 				
-
+				
 				for eachDay in weather {
 					
-				let day = Day()
+					let day = Day()
 					
 					guard let date = eachDay["date"] as? String else {
 						print("Cannot find key 'date' in \(weather)")
@@ -115,38 +120,41 @@ class CityWeatherViewController: UIViewController, UICollectionViewDataSource, U
 						print("Cannot find key 'minTempC' in \(weather)")
 						return
 					}
-
+					
 					guard let hourly = eachDay["hourly"] as? [[String: AnyObject]] else {
 						print("Cannot find key 'hourly' in \(weather)")
 						return
 					}
-
+					
 					for hour in hourly {
 						guard let tempC = hour["tempC"] as? String else {
 							print("Cannot find key 'tempC' in \(hourly)")
 							return
 						}
 						day.maxTemp_hours.append(tempC)
-
-					/*
+						
+						/*
 						for icon in hour {
-							guard let weatherIconUrl = icon["weatherIconUrl"] as? [[String: AnyObject]] else {
-								print("Cannot find key 'hourly' in \(weather)")
-								return
-							}
+						guard let weatherIconUrl = icon["weatherIconUrl"] as? [[String: AnyObject]] else {
+						print("Cannot find key 'hourly' in \(weather)")
+						return
 						}
-					*/
-
+						}
+						*/
+						
 					}
-
+					
 					day.date = date
 					day.maxTempC = maxTempC
 					day.minTempC = minTempC
-
-					self.city.forecast.append(day)
 					
-					self.performUIUpdatesOnMain({ 
-					self.collectionView.reloadData()
+					self.city.forecast.append(day)
+//					self.tempForecast.append(day)
+					
+					self.performUIUpdatesOnMain({
+						self.collectionView.reloadData()
+						
+						self.tableView.reloadData()
 					})
 				}
 			}
@@ -154,30 +162,6 @@ class CityWeatherViewController: UIViewController, UICollectionViewDataSource, U
 	}
 	
 	
-	//MARK: collection view datasource methods
-	
-	func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-		return 1
-	}
-	
-	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 8
-	}
-	
-
-	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-
-		let cell = collectionView.dequeueReusableCellWithReuseIdentifier("TodaysForecastCell", forIndexPath: indexPath) as! TodaysForecastCell
-
-		if let day = city.forecast.first {
-			let maxTempHours = day.maxTemp_hours
-				cell.temperature.text = "\(maxTempHours[indexPath.row])º"
-				cell.hour.text = day.times[indexPath.row]
-			}
-		 return cell
-		}
-
-
 	// MARK: helper methods
 	
 	func performUIUpdatesOnMain(updates: () -> Void) {
@@ -187,6 +171,59 @@ class CityWeatherViewController: UIViewController, UICollectionViewDataSource, U
 	}
 	
 	func alertViewForError(error: NSError) {
-	//
+		//
 	}
+}
+
+
+//MARK: collection view extension
+
+extension CityWeatherViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+	
+	func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+		return 1
+	}
+	
+	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 8
+	}
+	
+	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+		
+		let cell = collectionView.dequeueReusableCellWithReuseIdentifier("TodaysForecastCell", forIndexPath: indexPath) as! TodaysForecastCell
+		
+		if let day = city.forecast.first {
+			let maxTempHours = day.maxTemp_hours
+			cell.temperature.text = "\(maxTempHours[indexPath.row])º"
+			cell.hour.text = day.times[indexPath.row]
+		}
+		return cell
+	}
+
+}
+
+//MARK: table view extension
+
+extension CityWeatherViewController: UITableViewDelegate, UITableViewDataSource {
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+	
+		let day = city.forecast[indexPath.row]
+		let cell = tableView.dequeueReusableCellWithIdentifier("TableViewCell", forIndexPath: indexPath)
+		
+		let dayOfWeek = cell.viewWithTag(1001) as! UILabel
+		let maxTemp = cell.viewWithTag(1003) as! UILabel
+		let minTemp = cell.viewWithTag(1004) as! UILabel
+		
+		dayOfWeek.text = day.date
+		maxTemp.text = day.maxTempC
+		minTemp.text = day.minTempC
+	
+		return cell
+	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return city.forecast.count
+	}
+
 }
